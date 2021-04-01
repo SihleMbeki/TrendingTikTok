607e687a9a49c6a73878d093a30f86d13a5e609a
Masakhane Matanzima <sihle.matanzima@gmail.com>
2021-04-02 00:47:31 +0200

    Maven project setup

---
+++ src/main/java/TestNG/testautomation/App.java
@@ -0,0 +1,9 @@
+package TestNG.testautomation;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
---
+++ src/test/java/TestNG/testautomation/testing/AppTest.java
@@ -0,0 +1,12 @@
+package TestNG.testautomation.testing;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assert Boolean.TRUE;
+	}
+	
+}
