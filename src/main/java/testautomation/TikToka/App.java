511ec2ddea7d66cc73d7eb9ccc895f60034a2a40
Masakhane Matanzima <sihle.matanzima@gmail.com>
2021-04-13 22:34:30 +0200

    Initial Setup

---
+++ src/main/java/testautomation/TikToka/App.java
@@ -0,0 +1,9 @@
+package testautomation.TikToka;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
---
+++ src/test/java/TikToka/testing/AppTest.java
@@ -0,0 +1,12 @@
+package TikToka.testing;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assert Boolean.TRUE;
+	}
+	
+}
